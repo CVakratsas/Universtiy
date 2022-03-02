0b776877c4f8dacb7cc9d5ef56ec982c117726f6
Constantinos Vakratsas <98030260+CVakratsas@users.noreply.github.com>
2022-03-02 16:51:56 +0200

    v1

---
+++ Object_Oriented_Programming/Crime_Net/v1/CrimeNetGui/src/Registry.java
@@ -0,0 +1,101 @@
+import java.util.ArrayList;
+
+public class Registry {
+	
+	private ArrayList<Suspect> listOfSuspects = new ArrayList<>();
+	private ArrayList<Communication> listOfCommunications = new ArrayList<>();
+	
+	public ArrayList<Suspect> getListOfSuspects() {
+		return listOfSuspects;
+	}
+
+	public void addSuspect(Suspect aSuspect) {
+		listOfSuspects.add(aSuspect);
+	}
+	
+	public void addCommunication(Communication aCommunication) {
+		listOfCommunications.add(aCommunication);
+		Suspect sus1 = findSuspectFromPhoneNumber(aCommunication.getPhoneNumber1());
+		Suspect sus2 = findSuspectFromPhoneNumber(aCommunication.getPhoneNumber2());
+		sus1.addPartner(sus2);
+	}
+	
+	// βοηθητική συνάρτηση για την addCommunication
+	public Suspect findSuspectFromPhoneNumber(String phoneNumber) {
+		for(Suspect s : listOfSuspects) {
+			for(String p : s.getPhoneNumbersInUse()) {
+				if(p.equals(phoneNumber))
+					return s;
+			}
+		}
+		return null;
+	}
+	
+	public Suspect getSuspectWithMostPartners() {
+		Suspect suspectWithMostPartners = listOfSuspects.get(2); // για να δείχνει το ίδιο αποτέλεσμα με την ενδεικτική λύση, αφού όλοι οι ύποπτοι έχουν τον ίδιο αριθμό συνεργατών, 3
+		for(int i=0; i<listOfSuspects.size(); i++) {
+			if(listOfSuspects.get(i).getPartners().size() > suspectWithMostPartners.getPartners().size())
+				suspectWithMostPartners = listOfSuspects.get(i);
+		}
+		return suspectWithMostPartners;
+	}	
+	
+	public PhoneCall getLongestPhoneCallBetween(String number1, String number2) {
+		ArrayList<PhoneCall> phoneCallList = getPhoneCallList(number1, number2);
+		int index = -1;
+		int maxPhoneTimeInMinutes = 0;
+		for(int i=0; i<phoneCallList.size(); i++) {
+			if(phoneCallList.get(i).getPhoneCallTimeInMinutes() > maxPhoneTimeInMinutes) {
+				index = i;
+				maxPhoneTimeInMinutes = phoneCallList.get(i).getPhoneCallTimeInMinutes();
+			}
+		}
+		PhoneCall longestPhoneCall = (PhoneCall) listOfCommunications.get(index);
+		return longestPhoneCall;
+	}
+	
+	// βοηθητική μέθοδος της getLongestPhoneCallBetween, η οποία βρίσκει όλες τις τηλεφωνικές κλήσεις μεταξύ δύο αριθμών
+	public ArrayList<PhoneCall> getPhoneCallList(String number1, String number2){
+		ArrayList<PhoneCall> phoneCallList = new ArrayList<>();
+		for(Communication comm : listOfCommunications) {
+			if(comm instanceof PhoneCall) // επιλογή μόνο των αντικειμένων της κλάσης PhoneCall
+				if((number1.equals(comm.getPhoneNumber1()) && number2.equals(comm.getPhoneNumber2())) || (number1.equals(comm.getPhoneNumber2()) && (number2.equals(comm.getPhoneNumber1())))) {
+					PhoneCall phoneCall = (PhoneCall) comm; // ρητή μετατροπή σε κλάση PhoneCall από Communication
+					phoneCallList.add(phoneCall);
+				}
+		}
+		return phoneCallList;
+	}
+	
+	public ArrayList<SMS> getMessagesBetween(String number1, String number2){
+		ArrayList<SMS> listOfSMS = new ArrayList<>();
+		for(Communication comm : listOfCommunications) {
+			if(comm instanceof SMS) // επιλογή μόνο των αντικειμένων της κλάσης SMS
+				if((number1.equals(comm.getPhoneNumber1()) && number2.equals(comm.getPhoneNumber2())) || (number1.equals(comm.getPhoneNumber2()) && (number2.equals(comm.getPhoneNumber1())))) {
+					SMS sms = (SMS) comm;
+					if(sms.getSMSContent().contains("Bomb") || sms.getSMSContent().contains("Attack") || sms.getSMSContent().contains("Explosives") || sms.getSMSContent().contains("Gun"))
+						listOfSMS.add(sms);
+				}
+		}
+		return listOfSMS;
+	}
+	
+	public ArrayList<SMS> getMessages(String number) {
+		ArrayList<SMS> smsList = new ArrayList<>();
+		for(Communication com: listOfCommunications) {
+			if(com instanceof SMS) {
+				smsList.add((SMS) com);
+			}
+		}
+		ArrayList<SMS> selectedSMSList = new ArrayList<>();
+		for(SMS sms: smsList) {
+			if(number.equals(sms.getPhoneNumber1()) || (number.equals(sms.getPhoneNumber2())))
+				if(sms.getSMSContent().contains("Bomb") || 
+						sms.getSMSContent().contains("Attack") || 
+						sms.getSMSContent().contains("Explosives") || 
+						sms.getSMSContent().contains("Gun"))
+					selectedSMSList.add(sms);
+		}
+		return selectedSMSList;
+	}
+}
---
+++ Object_Oriented_Programming/Crime_Net/v1/CrimeNetGui/src/SuspectPageFrame.java
@@ -0,0 +1,196 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+
+public class SuspectPageFrame {
+		
+	Registry registry;
+	JFrame frame = new JFrame();
+	
+	// First section
+	JPanel infoPanel = new JPanel();
+	JTextField nameField = new JTextField();
+	JTextField codeNameField = new JTextField();
+	JTextArea numbersInUseArea = new JTextArea(4,10);
+	
+	// Second section
+	JPanel smsPanel = new JPanel();
+	JTextField phoneNumber = new JTextField();
+	JTextArea smsTextArea = new JTextArea(10,20);
+	JButton findSMSButton = new JButton("Find SMS");
+	
+	// Third section
+	JPanel partnersPanel = new JPanel();
+	JLabel partnersLabel = new JLabel("Partners");
+	JTextArea partnersTextArea = new JTextArea(10,20);
+	
+	// Forth section
+	JPanel suggestedPartnersPanel = new JPanel();
+	JLabel suggestedPartnersLabel = new JLabel("Suggested Partners");
+	JTextArea suggestedPartnersTextArea = new JTextArea(5,20);
+	
+	// Back to search button
+	JButton backToScreenButton = new JButton("Back To Search Screen");
+	
+	public SuspectPageFrame(Suspect suspect, Registry aRegistry) {
+		
+		registry = aRegistry;
+		ButtonListener listener = new ButtonListener();
+		Border border = BorderFactory.createLineBorder(Color.GRAY, 1);
+		
+		//------------------------ 1st panel ------------------------
+		nameField.setText(suspect.getName());
+		codeNameField.setText(suspect.getCodeName());
+		for(String number: suspect.getPhoneNumbersInUse()) {
+			numbersInUseArea.append(number + "\n");
+		}
+		
+		nameField.setEditable(false);
+		nameField.setFont(new Font(null, Font.PLAIN, 16));
+		nameField.setBackground(new Color(0xe8e8e8));
+		nameField.setPreferredSize(new Dimension(100,25));
+		
+		codeNameField.setEditable(false);
+		codeNameField.setFont(new Font(null, Font.PLAIN, 16));
+		codeNameField.setBackground(new Color(0xe8e8e8));
+		codeNameField.setPreferredSize(new Dimension(100,25));
+		
+		numbersInUseArea.setEditable(false);
+		numbersInUseArea.setFont(new Font(null, Font.PLAIN, 16));
+		numbersInUseArea.setBackground(new Color(0xe8e8e8));
+
+		infoPanel.add(nameField);
+		infoPanel.add(codeNameField);
+		infoPanel.add(numbersInUseArea);
+		
+		//------------------------ 2nd panel ------------------------
+		phoneNumber.setEditable(true);
+		phoneNumber.setFont(new Font(null, Font.PLAIN, 16));
+		phoneNumber.setPreferredSize(new Dimension(150,25));
+		phoneNumber.setText("Enter Number");
+		
+		smsTextArea.setEditable(false);
+		smsTextArea.setFont(new Font(null, Font.PLAIN, 16));
+		smsTextArea.setBackground(new Color(0xe8e8e8));
+		
+		findSMSButton.addActionListener(listener);
+		
+		smsPanel.add(phoneNumber);
+		smsPanel.add(smsTextArea);
+		smsPanel.add(findSMSButton);
+		
+		//------------------------ 3rd panel ------------------------
+		partnersLabel.setFont(new Font(null, Font.BOLD, 16));
+		partnersLabel.setPreferredSize(new Dimension(75,25));
+		
+		partnersTextArea.setFont(new Font(null, Font.PLAIN, 16));
+		partnersTextArea.setEditable(false);
+		partnersTextArea.setBackground(new Color(0xe8e8e8));
+		for(Suspect partner: suspect.getPartners())
+			partnersTextArea.append(partner.getName() + ", " + partner.getCodeName() + "\n");
+		
+		partnersPanel.add(partnersLabel);
+		partnersPanel.add(partnersTextArea);
+		
+		//------------------------ 4rth panel -----------------------
+		
+		suggestedPartnersLabel.setFont(new Font(null, Font.BOLD, 16));
+		suggestedPartnersLabel.setPreferredSize(new Dimension(170,25));
+		suggestedPartnersLabel.setBackground(Color.LIGHT_GRAY);
+		suggestedPartnersLabel.setOpaque(true);
+		
+		suggestedPartnersTextArea.setFont(new Font(null, Font.PLAIN, 16));
+		suggestedPartnersTextArea.setEditable(false);
+		suggestedPartnersTextArea.setBackground(new Color(0xe8e8e8));
+		for(Suspect suggestedPartner: suspect.getSortedSuggestedPartners())
+			suggestedPartnersTextArea.append(suggestedPartner.getName() + ", " + suggestedPartner.getCodeName() + "\n");
+		
+		suggestedPartnersPanel.add(suggestedPartnersLabel);
+		suggestedPartnersPanel.add(suggestedPartnersTextArea);
+		
+		backToScreenButton.addActionListener(listener);
+		
+		//------------------------ Frame -----------------------------
+		infoPanel.setBackground(Color.LIGHT_GRAY);
+		infoPanel.setBorder(border);
+		smsPanel.setBackground(Color.LIGHT_GRAY);
+		smsPanel.setBorder(border);
+		partnersPanel.setBackground(Color.LIGHT_GRAY);
+		partnersPanel.setBorder(border);
+		suggestedPartnersPanel.setBackground(Color.LIGHT_GRAY); // purple 0xa7a3d1
+		suggestedPartnersPanel.setBorder(border);
+		frame.getContentPane().setBackground(Color.LIGHT_GRAY); // light purple
+		
+		frame.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
+		
+		frame.add(infoPanel);
+		frame.add(smsPanel);
+		frame.add(partnersPanel);
+		frame.add(suggestedPartnersPanel);
+		frame.add(backToScreenButton);
+		
+		ImageIcon frameIcon = new ImageIcon("moving.jpg");
+		frame.setIconImage(frameIcon.getImage());
+		frame.setTitle("Suspect Page");
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600,800);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+	}
+
+	class ButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			final int PHONESIZE = 14;
+			
+			if(e.getSource().equals(backToScreenButton)) {
+				frame.dispose();
+				new LaunchPage(registry);
+			}
+			else if (e.getSource().equals(findSMSButton)) {
+				smsTextArea.setText(null);
+				String inputText = phoneNumber.getText();
+				if(inputText.length() == PHONESIZE || isNumeric(inputText)) {
+					ArrayList<SMS> smsList = registry.getMessages(phoneNumber.getText());
+					for(SMS sms: smsList) {
+						smsTextArea.append(sms.getSMSContent() + "\n");
+					}
+				}
+				else {
+					System.out.println("INVALID INPUT");
+				}
+			}
+			
+		}
+		
+		public static boolean isNumeric(String str) {
+			if (str == null) {
+				return false;
+			}
+			try {
+				int num = Integer.parseInt(str);
+			}
+			catch (NumberFormatException nfe) {
+				return false;
+			}
+			return true;
+		}		
+	}
+}
